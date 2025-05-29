c1cf65c357cecc1b1728f05197f5c914f3d86244
starkmage <zhang_shengjie@qq.com>
2025-05-29 21:06:41 +0800

    refine quicksort

--- 排序算法/快速排序-三分基准.js
+++ 排序算法/快速排序-三分基准.js
@@ -1,51 +1,83 @@
-function swap(i, j, array) {
-  let temp = array[i]
-  array[i] = array[j]
-  array[j] = temp
+/**
+ * 基于三分法选择基准值的快速排序
+ * @param {Array} arr 待排序数组
+ * @param {number} [left=0] 左边界索引
+ * @param {number} [right=arr.length-1] 右边界索引
+ * @returns {Array} 排序后的数组
+ */
+function quickSortThreeWay(arr, left = 0, right = arr.length - 1) {
+  if (left >= right) return arr;
+  
+  // 选择基准值（这里使用三数取中法）
+  const pivotIndex = getPivotIndex(arr, left, right);
+  const pivot = arr[pivotIndex];
+  
+  // 三分区间的边界初始化
+  let lt = left;      // 小于区的右边界
+  let gt = right;     // 大于区的左边界
+  let i = left;       // 当前元素指针
+  
+  while (i <= gt) {
+      if (arr[i] < pivot) {
+          // 当前元素小于基准值，放入小于区
+          swap(arr, i, lt);
+          lt++;
+          i++;
+      } else if (arr[i] > pivot) {
+          // 当前元素大于基准值，放入大于区
+          swap(arr, i, gt);
+          gt--;
+          // 注意这里不移动i，因为交换后的元素还未检查
+      } else {
+          // 当前元素等于基准值，留在等于区
+          i++;
+      }
+  }
+  
+  // 递归排序小于区和大于区
+  quickSortThreeWay(arr, left, lt - 1);
+  quickSortThreeWay(arr, gt + 1, right);
+  
+  return arr;
 }
 
-function quickSort(array, left, right) {
-  if (left >= right) return
-
-  //1.三分基准选择
-  let center = Math.floor((left + right) / 2)
-  if (array[left] > array[center]) {
-    swap(left, center, array)
+/**
+* 获取基准值索引（三数取中法）
+* @param {Array} arr 数组
+* @param {number} left 左边界
+* @param {number} right 右边界
+* @returns {number} 基准值索引
+*/
+function getPivotIndex(arr, left, right) {
+  const mid = Math.floor((left + right) / 2);
+  
+  // 找出左、中、右三个元素的中值
+  if (arr[left] > arr[mid]) {
+      swap(arr, left, mid);
   }
-  if (array[center] > array[right]) {
-    swap(center, right, array)
+  if (arr[left] > arr[right]) {
+      swap(arr, left, right);
   }
-  if (array[left] > array[center]) {
-    swap(left, center, array)
+  if (arr[mid] > arr[right]) {
+      swap(arr, mid, right);
   }
-
-  swap(left, center, array)
-
-  //2.快速排序
-  let temp = array[left]
-  let start = left
-  let end = right
-  while (left < right) {
-    while (left < right && array[right] >= temp) right--
-    array[left] = array[right]
-    while (left < right && array[left] <= temp) left++
-    array[right] = array[left]
-  }
-  array[left] = temp
-
-  //3.递归
-  quickSort(array, start, left - 1)
-  quickSort(array, left + 1, end)
+  
+  // 将中值放在right-1位置，作为基准值
+  swap(arr, mid, right - 1);
+  return right - 1;
 }
 
-let a = [3, 2, 1, 4, 9, 2, 1, 13]
+/**
+* 交换数组中的两个元素
+* @param {Array} arr 数组
+* @param {number} i 索引1
+* @param {number} j 索引2
+*/
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
 
-quickSort(a, 0, a.length - 1)
-
-let b = [7, 6, 8, 5, 4, 3, 2, 1]
-
-quickSort(b, 0, b.length - 1)
-
-console.log(a);
-
-console.log(b);
+// 使用示例
+const array = [3, 7, 8, 5, 2, 1, 9, 5, 4];
+console.log('排序前:', array);
+console.log('排序后:', quickSortThreeWay(array));
